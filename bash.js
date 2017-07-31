8b138ab9bd434615d43a812634524c29054f447a
Mary Yen <mymaryyn@gmail.com>
2017-07-31 12:52:09 -0400

    refactoring

--- bash.js
+++ bash.js
@@ -2,6 +2,11 @@
 
 var commands = require('./commands');
 var userCommand = 'pwd';
-commands[userCommand]();
+
+process.stdin.on('data', function (data) {
+    var cmd = data.toString().trim().split(' '); // remove the newline
+    commands[cmd[0]]();
+})
+
 
 // The stdin 'data' event fires after a user types in a line
--- commands.js
+++ commands.js
@@ -1,6 +1,38 @@
 var fs = require('fs');
 var chalk = require('chalk');
 process.stdout.write(chalk.yellow('prompt > '));
+
+var done = function(output) {
+  process.stdout.write(chalk.blue(output));
+  process.stdout.write(chalk.yellow('\nprompt > '));
+}
+
+var commands = {
+  done: function(output) {
+  process.stdout.write(chalk.blue(output));
+  process.stdout.write(chalk.yellow('\nprompt > '));
+},
+  ls: function() {
+    var output = "";
+    fs.readdir('.', function(err, files) {
+      files.forEach(function(file) {
+        output += file.toString() + "\n";
+      })
+      done(output);
+    });
+  },
+  pwd: function() {
+    var output = "";
+    fs.readdir('.', function(err, files) {
+      files.forEach(function(file) {
+        output += file.toString() + "\n";
+      })
+      done(output);
+    });
+  }
+
+}
+
 var obj = {
     pwd: function(){
       process.stdin.on('data', function (data) {
@@ -10,7 +42,10 @@ var obj = {
       } else if(cmd[0] == "date") {
         process.stdout.write(chalk.cyan((new Date()).toString()));      
       } else if(cmd[0] == "ls") {
-        process.stdout.write(chalk.red(fs.readdirSync('.').toString().replace(/,/g , " ")));
+        chalk.red(fs.readdir('.', function(err, file){
+          if(err) throw err;
+          process.stdout.write(file.toString().replace(/,/g , " "));
+        }));
       } else if(cmd[0] == "echo") {
         if(cmd[1][0] == "$"){
           process.stdout.write(chalk.magenta(process.env[cmd[1].slice(1)]));
@@ -24,12 +59,19 @@ var obj = {
       }  else if(cmd[0] == "head") {
         fs.readFile(cmd[1], 'utf8', function(err, file){
           if(err) throw err;
+          var file_array = file.split('\n');
           for(var i = 0; i < 5; i++){
-          console.log(file.split('\n')[i]);
+          console.log(file_array[i]);
           }
         })
-      } else if() {
-        
+      } else if(cmd[0] == "tail") {
+        fs.readFile(cmd[1], 'utf8', function(err, file){
+          if(err) throw err;
+          var file_array = file.split('\n');
+          for(var i = file_array.length-5; i < file_array.length; i++){
+          console.log(file_array[i]);
+          }
+        })
       } 
       else {
         process.stdout.write('You typed: ' + cmd);
@@ -39,4 +81,4 @@ var obj = {
     }
 }
 
-module.exports=obj
+module.exports=commands
