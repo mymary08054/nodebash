501e2cb041ce0f82df8ef595e2098408a22dd345
Li Wang <esmbug@gmail.com>
2017-07-31 11:40:23 -0400

    echo

--- commands.js
+++ commands.js
@@ -3,14 +3,18 @@ var fs = require('fs');
 var obj = {
     pwd: function(){
       process.stdin.on('data', function (data) {
-      var cmd = data.toString().trim(); // remove the newline
-
-      if(cmd == "pwd") {
+      var cmd = data.toString().trim().split(' '); // remove the newline
+      if(cmd[0] == "pwd") {
         process.stdout.write(process.cwd());
-      } else if(cmd == "date") {
+      } else if(cmd[0] == "date") {
         process.stdout.write((new Date()).toString());      
-      } else if(cmd == "ls") {
+      } else if(cmd[0] == "ls") {
         process.stdout.write(fs.readdirSync('.').toString().replace(/,/g , " "));
+      } else if(cmd[0] == "echo") {
+        if(cmd[1] == "$PATH"){
+          process.stdout.write(process.env.PATH);
+        }
+        else process.stdout.write(cmd.slice(1).toString().replace(/,/g , " "));
       } else {
         process.stdout.write('You typed: ' + cmd);
       }
