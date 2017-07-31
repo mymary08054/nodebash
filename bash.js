74ac4bdc26bcc0541af3690c079213344611b05f
Mary Yen <mymaryyn@gmail.com>
2017-07-31 12:04:44 -0400

    echo stuff

--- bash.js
+++ bash.js
@@ -4,6 +4,4 @@ var commands = require('./commands');
 var userCommand = 'pwd';
 commands[userCommand]();
 
-process.stdout.write('prompt > ');
-
 // The stdin 'data' event fires after a user types in a line
--- commands.js
+++ commands.js
@@ -1,24 +1,27 @@
 var fs = require('fs');
+var chalk = require('chalk');
+var log = console.log;
 
+process.stdout.write(chalk.yellow('prompt > '));
 var obj = {
     pwd: function(){
       process.stdin.on('data', function (data) {
       var cmd = data.toString().trim().split(' '); // remove the newline
       if(cmd[0] == "pwd") {
-        process.stdout.write(process.cwd());
+        process.stdout.write(chalk.blue(process.cwd()));
       } else if(cmd[0] == "date") {
-        process.stdout.write((new Date()).toString());      
+        process.stdout.write(chalk.cyan((new Date()).toString()));      
       } else if(cmd[0] == "ls") {
-        process.stdout.write(fs.readdirSync('.').toString().replace(/,/g , " "));
+        process.stdout.write(chalk.red(fs.readdirSync('.').toString().replace(/,/g , " ")));
       } else if(cmd[0] == "echo") {
-        if(cmd[1] == "$PATH"){
-          process.stdout.write(process.env.PATH);
+        if(cmd[1][0] == "$"){
+          process.stdout.write(chalk.magenta(process.env[cmd[1].slice(1)]));
         }
         else process.stdout.write(cmd.slice(1).toString().replace(/,/g , " "));
       } else {
         process.stdout.write('You typed: ' + cmd);
       }
-        process.stdout.write('\nprompt > ');
+        process.stdout.write(chalk.yellow('\nprompt > '));
       });
     }
 }
