443ebb3a4022147ef8648db9b60bb79cd22e6d91
Mary Yen <mymaryyn@gmail.com>
2017-07-31 11:20:03 -0400

    ls

--- commands.js
+++ commands.js
@@ -1,19 +1,21 @@
-
+var fs = require('fs');
 
 var obj = {
     pwd: function(){
-process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
+      process.stdin.on('data', function (data) {
+      var cmd = data.toString().trim(); // remove the newline
 
-  if(cmd == "pwd") {
-    process.stdout.write(process.cwd());
-  } else if(cmd == "date") {
-    process.stdout.write((new Date()).toString());      
-  } else {
-    process.stdout.write('You typed: ' + cmd);
-  }
-    process.stdout.write('\nprompt > ');
-});
+      if(cmd == "pwd") {
+        process.stdout.write(process.cwd());
+      } else if(cmd == "date") {
+        process.stdout.write((new Date()).toString());      
+      } else if(cmd == "ls") {
+        process.stdout.write(fs.readdirSync('.').toString().replace(/,/g , " "));
+      } else {
+        process.stdout.write('You typed: ' + cmd);
+      }
+        process.stdout.write('\nprompt > ');
+      });
     }
 }
 
